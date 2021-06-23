da2847ef372e3078772e1b0d92ca509a9433ef94
Jaeuk <didwodnr123@gmail.com>
2021-06-23 20:37:35 +0900

    Update review.py

--- review.py
+++ review.py
@@ -1,50 +1,17 @@
-def snail(N):
-    global cnt
-    global x, y
-    
-    while N > 0:
-        N -= 1
-        for _ in range(N):
-            x += dx[0]
-            y += dy[0]
-            if lst[x][y] != 0:
-                x -= dx[0]
-                y -= dy[0]
-                break
-            cnt += 1
-            lst[x][y] = cnt
-        for _ in range(N):
-            x += dx[1]
-            y += dy[1]
-            if lst[x][y] != 0:
-                x -= dx[1]
-                y -= dy[1]
-                break
-            cnt += 1
-            lst[x][y] = cnt
-        N -= 1
-        for _ in range(N):
-            x += dx[2]
-            y += dy[2]
-            if lst[x][y] != 0:
-                x -= dx[2]
-                y -= dy[2]
-                break
-            cnt += 1
-            lst[x][y] = cnt
-
 N = int(input())
+dp_list = [0,0,1,1] # 0 ,1, 2, 3 의 최소 수 미리 저장
 
-lst = [[0] * N for _ in range(N)]
-print(lst)
-# 아래대각, 위, 오른쪽
-dx = [1, -1, 0]
-dy = [-1, 0, 1]
+for i in range(4, N + 1) :
+    # 먼저 1을 뺏을 경우 나오는 경우의 수 저장
+    dp_list.append(dp_list[i-1] + 1)
 
-x = 0
-y = N-1
-cnt = 1
-lst[x][y] = cnt
-snail(N)
-print(lst)
+    #2로 나누어질 경우 기존 1을 뺏을 경우의 수와 비교하여 최솟값 저장
+    if i % 2 == 0 :
+        dp_list[i] = min(dp_list[i], dp_list[i//2] + 1)
 
+    #3으로 나누어질 경우 기존 1을 뺏을 경우의 수와 비교하여 최솟값 저장
+    #여기서 2 또는 3으로 나누어질 경우 모든 경우를 봐야하므로 elif가 아닌 if로 설정
+    if i % 3 == 0 :
+        dp_list[i] = min(dp_list[i], dp_list[i//3] + 1)
+
+print(dp_list[-1])
