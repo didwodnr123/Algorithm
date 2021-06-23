80fd50207da4fe89771d1b08646b4075a232735b
Jaeuk <didwodnr123@gmail.com>
2021-06-23 13:36:54 +0900

    Update

--- review.py
+++ review.py
@@ -1,30 +1,37 @@
-from collections import deque
+def dfs(x, y):
+    field[x][y] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx<0 or ny<0 or nx>h-1 or ny>w-1:
+            continue
+        if field[nx][ny] == 0:
+            continue
+        if field[nx][ny] == 1:
+            dfs(nx, ny)
 
-N, M = map(int, input().split())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input())))
+answer = []
 
-# 상 하 좌 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx > N-1 or ny > M-1:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return graph[N-1][M-1]
-    
-print(bfs(0, 0))
-                 
+    field = []
+    for j in range(h):
+        field.append(list(map(int, input().split())))
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    cnt = 0
+
+    for i in range(h):
+        for j in range(w):
+            if field[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+
+    answer.append(cnt)
+
+for ans in answer:
+    print(ans)
