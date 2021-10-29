2b34cdf67b4954b5844ebc008785c711648f7bf6
Jadon <didwodnr123@gmail.com>
2021-10-29 10:31:29 +0900

    Update 1931.py

--- Greedy/1931.py
+++ Greedy/1931.py
@@ -1,19 +1,20 @@
-n = int(input())
-times = []
+t = int(input())
+lst = []
+for _ in range(t):
+    a, b = map(int, input().split())
+    lst.append((a,b))
 
-for i in range(n):
-    start, end = map(int, input().split())
-    times.append([start, end])
+lst = sorted(lst, key=lambda x: x[0])
+lst = sorted(lst, key=lambda x: x[1])
 
-times = sorted(times, key=lambda x: x[0])
-times = sorted(times, key=lambda x: x[1])
-
-last = 0
-answer = []
-
-for s, e in times:
-    if s >= last:
-        answer.append([s,e])
-        last = e
-
-print(len(answer))
+prev_start, prev_end, cnt = 0, 0, 0
+# 시작시간 기준으로 정렬
+for i in lst:
+    start = i[0]
+    end = i[1]
+    if start >= prev_end:
+        cnt += 1
+        prev_end = end
+        
+print(cnt)
+    
