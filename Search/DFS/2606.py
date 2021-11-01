87fc208f1e974bea5836e548566e65ef60bdc5b4
Jadon <didwodnr123@gmail.com>
2021-11-01 23:07:19 +0900

    Update 2606.py

--- Search/DFS/2606.py
+++ Search/DFS/2606.py
@@ -1,19 +1,35 @@
-N = int(input())
-n_edge = int(input())
+from collections import deque
 
-graph = [[0] * (N+1) for _ in range(N+1)]
-visited = [False] * (N+1)
+def bfs():
+    queue = deque()
+    queue.append(1)
+    bfs_visited[1] = True
+    while queue:
+        q = queue.popleft()
+        for i in range(n+1):
+            if not bfs_visited[i] and graph[q][i] == 1:
+                bfs_visited[i] = True
+                queue.append(i)
 
-for _ in range(n_edge):
+def dfs(v):
+    dfs_visited[v] = True
+    for i in range(n+1):
+        if not dfs_visited[i] and graph[v][i] == 1:
+            dfs(i)
+    
+
+n = int(input())
+m = int(input())
+graph = [[0] * (n+1) for _ in range(n+1)]
+bfs_visited = [False] * (n+1)
+dfs_visited = [False] * (n+1)
+
+for _ in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
-
-def dfs(v):
-    visited[v] = True
-    for i in range(1, N+1):
-        if not visited[i] and graph[v][i] == 1:
-            dfs(i)
-
+    
+bfs()
+print(sum(bfs_visited) - 1)
 dfs(1)
-print(visited.count(True) - 1)
+print(sum(dfs_visited) - 1)
