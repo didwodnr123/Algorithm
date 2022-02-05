b1233f1ab2a90d6e22cb6315ce0244bf1bacc150
Jadon <didwodnr123@gmail.com>
2022-02-05 16:22:51 +0900

    UPDATE

--- coding-interview/string/5-longest-palindromic-substring.py
+++ coding-interview/string/5-longest-palindromic-substring.py
@@ -1,12 +1,19 @@
-import collections
-from typing import List
-
-
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = collections.defaultdict(list)
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-        for word in strs:
-            anagrams[''.join(sorted(word))].append(word)
+        if len(s) < 2 or s == s[::-1]:
+            return s
 
-        return anagrams.values()
+        result = ''
+        for i in range(len(s) - 1):
+            result = max(result,
+                         expand(i, i + 1),
+                         expand(i, i + 2),
+                         key=len)
+
+        return result
